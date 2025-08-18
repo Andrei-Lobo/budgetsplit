a44de33bbc6489d6e2fc88dbb55dd4ae8b61a0c0
Andrei Christian Lobo <andrei.christian.lobo@gmail.com>
2025-08-18 19:15:47 +0800

    added User getters and setters

---
+++ src/main/java/com/budgetsplittingapp/budgetsplit/model/Expense.java
@@ -0,0 +1,5 @@
+package com.budgetsplittingapp.budgetsplit.model;
+
+public class Expense {
+    
+}
---
+++ src/main/java/com/budgetsplittingapp/budgetsplit/model/User.java
@@ -0,0 +1,41 @@
+package com.budgetsplittingapp.budgetsplit.model;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
