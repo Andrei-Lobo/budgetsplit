0bc1284b2d48e00d9393bf89ba3478131a364fdc
Andrei Christian Lobo <andrei.christian.lobo@gmail.com>
2025-08-18 17:54:46 +0800

    added

---
+++ src/main/java/com/budgetsplittingapp/budgetsplit/BudgetsplitApplication.java
@@ -0,0 +1,13 @@
+package com.budgetsplittingapp.budgetsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetsplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetsplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgetsplittingapp/budgetsplit/BudgetsplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetsplittingapp.budgetsplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetsplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
